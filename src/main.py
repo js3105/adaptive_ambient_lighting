989a098b0fcb1c26567cbec5b920a13e8238df8b
js3105 <j.schneider030105@gmail.com>
2025-08-14 09:32:41 +0200

    Include Fix 3

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
-from camera.camera_manager import CameraManager
-from detection.detector import ObjectDetector
+from camera/camera_manager import CameraManager
+from detection/detector import ObjectDetector
 
 def main():
     try:
